4d8b6270b5c989e4f9b756f66420f3961f27875f
DMITRIY USTINOV <48724054+Ustincheg@users.noreply.github.com>
2020-07-21 20:24:55 +0300

    New commit

--- docs/js/app.js
+++ docs/js/app.js
@@ -38,4 +38,56 @@ $(document).ready(function(){
 		focusOnSelect: true,
 		arrows: false	  
 	  });
-  });	
+  });
+  
+  $(document).ready(function(){
+	$('.slick-slider-wrap').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		autoplay: true,
+	});
+  });
+
+
+  
+ 
+let amount = 10000;
+const multiplier = () => {
+	const materialFacade = Number(document.getElementById("materialFacade").value);
+	const tableTopMaterial = Number(document.getElementById("tableTopMaterial").value);
+	const hardwareClass = Number(document.getElementById("hardwareClass").value);
+	
+
+	// const radio = document.getElementsByClassName("radio-JS")
+	// const radiola = radio.querySelectorAll(input[checked]).value;
+
+	
+
+	amount = materialFacade * tableTopMaterial * hardwareClass;
+	qwerqwe.innerHTML = parseInt(amount);
+	return amount;
+}
+const constructorСalculation = () => {	
+	calcResult.innerHTML = parseInt(amount);
+};
+
+function calc() {
+	//получаем ссылку на элемент Select (Тип дизайна)
+	var type_design = document.getElementById("type_design");
+	//получаем ссылку на чекбокс (Требуется верстка?)
+	var is_html = document.getElementById("is_html");
+	//получаем ссылку на элемент input (Кол-во вариантов)
+	var count = document.getElementById("count");
+	//получаем ссылку на элемент span, в него будем писать стоимость дизайна
+	var result = document.getElementById("result"); 
+
+	var price = 0;
+	price += parseInt(type_design.options[type_design.selectedIndex].value);
+	price += (is_html.checked == true) ? parseInt(is_html.value) : 0;
+	price = parseInt(count.value) * price;
+
+	result.innerHTML = price;
+}
